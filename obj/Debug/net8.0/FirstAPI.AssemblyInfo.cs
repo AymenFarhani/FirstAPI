070fed8580bf818c8d0b5fe84d211b5956f2b260
RabebJaouadi <aymenfarhani28@gmail.com>
2025-05-23 09:03:49 +0200

    Update documentation

--- obj/Debug/net8.0/FirstAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55cdbdeb0988483f05d3902d09869e0cc8a754b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33812448a0de0eba16d8a9c8334f1e2270aedff")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
